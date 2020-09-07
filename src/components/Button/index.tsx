9cd3f8fda1942227dd3547f99535ae6418f2b1ed
Jefferson Botelho <jgbotelho3@outlook.com>
2020-09-07 13:54:48 -0300

    Create Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,25 @@
+import * as S from './styles'
+
+export type ButtonProps = {
+  children?: React.ReactNode
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: React.ReactNode
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
+
+}
+
+const Button = ({
+  children,
+  size = 'medium',
+  fullWidth = false,
+  icon,
+  ...props
+  }: ButtonProps) => (
+  <S.Wrapper size = {size} fullWidth = {fullWidth} hasIcon={!!icon} {...props}>
+    {icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default Button
---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,29 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import {AddShoppingCart} from '@styled-icons/material-outlined/AddShoppingCart'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon:{
+      type: ''
+    }
+  }
+} as Meta
+
+export const Default: Story = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy Now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
+}
