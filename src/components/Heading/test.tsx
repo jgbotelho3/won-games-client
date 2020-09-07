e50b0556fe58bfd13c1ead159711ec4017a1d1c5
Jefferson Botelho <jgbotelho3@outlook.com>
2020-09-07 19:57:37 -0300

    Adjust Heading component

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -21,7 +21,7 @@ describe('<Heading />', () => {
   it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
-     'border-left': '0.7rem solid #3CD3C1'
+     'border-left': '0.7rem solid #F231A5'
     })
   })
 
@@ -34,5 +34,36 @@ describe('<Heading />', () => {
         modifier: '::after'
       }
     )
+  })
+
+  it('should render a heading with a small size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
+
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('Should be render a Heading with a secondary line color', () => {
+    renderWithTheme(
+    <Heading lineColor="secondary" lineLeft lineBottom>
+      Most Important
+    </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /Most Important/i})
+    expect(heading).toHaveStyle({'border-left': '0.7rem solid #3CD3C1'})
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier:
+        '::after'
+    })
   })
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Main from 'components/Main'
+import Menu from 'components/Menu'
 
 export default function Home() {
-  return <Main />
+  return <Menu />
 }
