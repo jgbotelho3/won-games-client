ef91bc76d87dcd4dda7e294ca47c46cd2c6afd78
Jefferson <jgbotelho3@outlook.com>
2020-09-28 23:40:11 -0300

    Banner + Ribbon

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,11 +1,15 @@
 import * as S from './styles'
 import Button from 'components/Button'
+import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
 export type BannerProps = {
   img: string
   title: string
   subtitle: string
   buttonLabel: string
   buttonLink: string
+  ribbon?: React.ReactNode
+  ribbonColor?: RibbonColors
+  ribbonSize?: RibbonSizes
 }
 
 const Banner = ({
@@ -13,9 +17,17 @@ const Banner = ({
   title,
   subtitle,
   buttonLabel,
-  buttonLink
+  buttonLink,
+  ribbon,
+  ribbonColor = 'primary',
+  ribbonSize = 'normal'
 }: BannerProps) => (
   <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon color={ribbonColor} size={ribbonSize}>
+        {ribbon}
+      </Ribbon>
+    )}
     <S.Image src={img} role="img" aria-label={title} />
 
     <S.Caption>
