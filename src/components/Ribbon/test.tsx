60dddd7ee329d31c5816bbfeaf8f288350c71037
Jefferson <jgbotelho3@outlook.com>
2020-09-28 23:07:54 -0300

    Ribbon Finished

---
+++ src/components/Ribbon/test.tsx
@@ -0,0 +1,46 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Ribbon from '.'
+
+describe('<Ribbon />', () => {
+  it('should render the text correctly', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toBeInTheDocument()
+  })
+
+  it('Should render with the primary color', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      backgroundColor: '#F231A5'
+    })
+  })
+
+  it('Should render with the secondary color', () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+  })
+
+  it('Should render a normal size as default', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      height: '3.6rem',
+      fontSize: '1.4rem'
+    })
+  })
+
+  it('Should render a small', () => {
+    renderWithTheme(<Ribbon size="small">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
+})
