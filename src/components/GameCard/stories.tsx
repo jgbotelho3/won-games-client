73feb25614a80c501122837b7d64c9f35581c3ad
Jefferson <jgbotelho3@outlook.com>
2020-10-20 12:23:47 -0300

    Create GameCard component

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,41 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameCard, { GameCardProps } from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population game',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-float.png',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  argTypes: {
+    onFav: { action: 'clicked' },
+    ribbon: { type: 'string' }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+export const WithRibbon: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% off',
+  ribbonSize: 'small',
+  ribbonColor: 'primary'
+}
