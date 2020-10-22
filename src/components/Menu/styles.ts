0c4b4172b228ad0244f7813bd5aacff7b4c8fafc
Jefferson <jgbotelho3@outlook.com>
2020-10-22 19:48:32 -0300

    Populate Home with mocks

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -51,7 +51,8 @@ export const MenuFull = styled.nav<MenuFullProps>`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    position: absolute;
+    position: fixed;
+    z-index: ${theme.layers.menu};
     top: 0;
     bottom: 0;
     left: 0;
