498b308998433d346034c058723aa717df362e00
Jefferson <jgbotelho3@outlook.com>
2020-11-15 04:51:21 -0300

    Create Checkbox component

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,69 @@
+import { screen, waitFor } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import userEvent from '@testing-library/user-event'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('Should be render without label', () =>{
+
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+
+  it('Should be render with black label', () =>{
+
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" labelColor="black"/>)
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('Should dispatch on check when label status change', async () =>{
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="Checkbox" onCheck={onCheck}/>)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+   await waitFor(() =>{
+     expect(onCheck).toHaveBeenCalledTimes(1)
+   })
+   expect(onCheck).toHaveBeenCalledWith(true)
+  })
+
+  it('Should dispacth oncheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="checkbox" onCheck={onCheck} isChecked />)
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() =>{
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(false)
+  })
+
+  it('Should be accessible with tab', () => {
+    renderWithTheme(<Checkbox label="Checkbox" labelFor="Checkbox" />)
+
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+
+    expect(screen.getByLabelText(/checkbox/i)).toHaveFocus()
+  })
+})
