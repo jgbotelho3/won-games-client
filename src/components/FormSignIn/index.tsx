3dc929e12a0bc73ba77eab5c3e3beeac553304f6
Jefferson <jgbotelho3@outlook.com>
2020-11-23 15:49:13 -0300

    Add Styles to Sign In form

---
+++ src/components/FormSignIn/index.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { Email, Lock } from '@styled-icons/material-outlined'
+import TextField from 'components/TextField'
+import Button from 'components/Button'
+import * as S from './styles'
+
+const FormSignIn = () => (
+  <S.Wrapper>
+    <form>
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+      <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+      <Button size="large" fullWidth>Sign In now</Button>
+
+      <S.FormLink>Don't have an account? <Link href="/sign-up"><a>Sign up</a></Link></S.FormLink>
+    </form>
+  </S.Wrapper>
+)
+
+export default FormSignIn
