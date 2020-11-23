8a06aae3a9f387bb09b36e479434a630400d11c2
Jefferson <jgbotelho3@outlook.com>
2020-11-23 16:18:05 -0300

    Finished SignUp page

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,6 @@
 import Auth from 'templates/Auth'
+import FormSignUp from 'components/FormSignUp'
 
 export default function SignUp() {
-  return <Auth title="Sign Up">Sign Up</Auth>
+  return <Auth title="Sign Up"><FormSignUp /></Auth>
 }
